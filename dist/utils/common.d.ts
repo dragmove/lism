8352cba754ff285a6716e82901c196422a1abc0f
hyunseok.kim <hyunseok.kim@42dot.ai>
2021-11-23 01:34:34 +0900

    chore: Add jsdoc annotations to isDefined function

--- dist/utils/common.d.ts
+++ dist/utils/common.d.ts
@@ -1,1 +1,12 @@
+/**
+ * check if a value is defined
+ *
+ * @function isDefined
+ * @param {?} val
+ * @returns {boolean}
+ * @example
+ * console.log(isDefined(undefined)); // false
+ * console.log(isDefined(null)); // false
+ * console.log(isDefined(0)); // true
+ */
 export declare function isDefined(val: unknown): boolean;
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,4 +1,16 @@
-// TODO: Add comments to make api documents
+// Ref: https://www.typescriptlang.org/docs/handbook/jsdoc-supported-types.html
+
+/**
+ * check if a value is defined
+ *
+ * @function isDefined
+ * @param {?} val
+ * @returns {boolean}
+ * @example
+ * console.log(isDefined(undefined)); // false
+ * console.log(isDefined(null)); // false
+ * console.log(isDefined(0)); // true
+ */
 export function isDefined(val: unknown): boolean {
   if (val === null || typeof val === "undefined") return false;
   return true;
