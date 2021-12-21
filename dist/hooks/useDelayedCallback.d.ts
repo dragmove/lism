210226512dea811388d7cdcca2dafa12acda327c
hyunseok.kim <hyunseok.kim@42dot.ai>
2021-12-22 03:36:52 +0900

    chore: Change version

--- dist/hooks/useDelayedCallback.d.ts
+++ dist/hooks/useDelayedCallback.d.ts
@@ -1,2 +1,12 @@
-declare const useDelayedCallback: (delay: number) => (callback: any) => any;
+/**
+ * Return a delayed callback custom hook
+ *
+ * @function useDelayedCallback
+ * @param {number} delay - millisecond
+ * @returns {(callback: () => unknown) => unknown}
+ * @example
+ * const delayedCallback = useDelayedCallback(1000);
+ * delayedCallback(() => console.log('foo'));
+ */
+declare const useDelayedCallback: (delay: number) => (callback: () => unknown) => unknown;
 export default useDelayedCallback;
