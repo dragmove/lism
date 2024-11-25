be1b283ae81ec3d0144d2d07fd55c6f0e4a502a9
Hyunseok Kim <dragmove@naver.com>
2024-11-25 17:36:41 +0900

    feat: arrange typedoc description of DelayedFallback comp

--- src/components/DelayedFallback/DelayedFallback.tsx
+++ src/components/DelayedFallback/DelayedFallback.tsx
@@ -1,23 +1,35 @@
 import React, { ReactNode } from 'react';
 import useDelayedCallback from '../../hooks/useDelayedCallback';
 
-interface PropTypes {
+/**
+ * The DelayedFallback component conditionally renders its children or a fallback component
+ * based on the `isReadyToRender` prop. It waits for a specified delay (fallbackDelay)
+ * before rendering the fallback component.
+ *
+ * @param {Object} props - The props for the component.
+ * @param {ReactNode} props.children - The child elements to render.
+ * @param {boolean} props.isReadyToRender - A flag indicating whether the children are ready to be rendered.
+ * @param {ReactNode} [props.fallback] - The fallback component to render when `isReadyToRender` is false.
+ * @param {number} [props.fallbackDelay=200] - The time (in milliseconds) to wait before rendering the fallback component.
+ * @returns {ReactNode} - Returns the children if ready, otherwise returns the fallback component.
+ */
+const DelayedFallback = ({
+  children,
+  isReadyToRender = false,
+  fallback,
+  fallbackDelay = 200,
+}: {
   children: ReactNode;
   isReadyToRender: boolean;
   fallback?: ReactNode;
+  /**
+   * @defaultValue 200
+   */
   fallbackDelay?: number;
-}
-
-/**
- * A React component that conditionally renders its children or a fallback component
- * based on the `isReadyToRender` prop. It uses a delay before rendering the fallback.
- *
- * @param {PropTypes} props - The component props.
- * @returns {ReactNode} - The rendered children or fallback content.
- */
-const DelayedFallback = ({ children, isReadyToRender = false, fallback, fallbackDelay = 200 }: PropTypes): any => {
+}): ReactNode => {
   const delayedCallback = useDelayedCallback(fallbackDelay);
-  return isReadyToRender ? <>{children}</> : delayedCallback(() => fallback);
+
+  return isReadyToRender ? <>{children}</> : delayedCallback(() => fallback || null) || null;
 };
 
 export default DelayedFallback;
